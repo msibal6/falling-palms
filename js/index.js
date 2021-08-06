252c4b2ffbee0651ccaaffbc8a2c2c73dca91264
Mitchell Sibal <mitchell.sibal@gmail.com>
2021-08-05 20:39:55 -0700

    clean up index after encapsulation

--- js/index.js
+++ js/index.js
@@ -61,6 +61,7 @@ class Game {
 				this.camera = orbitCamera;
 				game.threeManager.camera = threeCamera;
 			},
+			
 			update: function () {
 				if (keyboardController.pressed["w"]) {
 					if (this.xAcceleration > this.maxSpeed) {
@@ -117,57 +118,11 @@ const game = new Game();
 const keyboardController = new KeyboardController();
 keyboardController.init();
 
-// setting up the visual in three
-// Scene
-// const scene = game.threeManager.createScene();
-// Camera
-// const camera = new THREE.PerspectiveCamera(75,
-// 	window.innerWidth / window.innerHeight, 0.1, 1000);
-// Renderer
-// const renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.body.appendChild(renderer.domElement);
-// Resizing the window on resize 
-// might delete
-
-// TODO put this in three manager 
 window.addEventListener('resize', game.threeManager.handleWindowResize(), false);
-// function handleWindowResize() {
-// 	// update height and width of the renderer and the camera
-// 	console.log("resizing in index");
-// 	const windowHeight = window.innerHeight;
-// 	const windowWidth = window.innerWidth;
-// 	renderer.setSize(windowWidth, windowHeight);
-// 	camera.aspect = windowWidth / windowHeight;
-// 	camera.updateProjectionMatrix();
-// }
-
-// Placing a cube in the scene
-// const geometry = new THREE.BoxGeometry();
-// const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
-// const cube = new THREE.Mesh(geometry, material);
-
-// const player = {
-// 	// visual
-// 	// THREE mesh
-// 	mesh: null,
-// 	// physics
-// 	// cannon body
-// 	body: null,
-// 	maxSpeed: 20.0,
-// 	acceleration: 1.0,
-// 	xAcceleration: 0.0,
-// 	zAcceleration: 0.0,
-// 	damping: 0.9,
-// 	camera: null,
-
-// }
 
 // Scene
-
 // const axesHelper = new THREE.AxesHelper(5);
 // scene.add(axesHelper);
-
 // Adds red skybox
 const loader = new THREE.CubeTextureLoader();
 const texture = loader.load([
@@ -190,28 +145,11 @@ plane.receiveShadow = true;
 plane.rotation.x = -Math.PI / 2;
 game.threeManager.addToScene(plane);
 
-// Add visual player placeholder
-// const box = new THREE.Mesh(
-// 	new THREE.BoxGeometry(2, 2, 2),
-// 	new THREE.MeshLambertMaterial({
-// 		color: 0xFFFFFF,
-// 	}));
-// box.castShadow = true;
-// box.receiveShadow = true;
-// scene.add(box);
-// player.mesh = box;
-
-// Orbit camera tracks position of player mesh in the visual scene
-// const orbitCamera = new SphericalPanCamera(camera, player.mesh);
-// orbitCamera.setPhiPan(Math.PI, Math.PI);
-// orbitCamera.setThetaPan(Math.PI / 4 * 3, Math.PI / 4);
-// orbitCamera.setRadius(10);
-// player.camera = orbitCamera;
-
-// Add light
+// Add ambient light
 const ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
 game.threeManager.addToScene(ambientLight);
 
+// Add sunlight
 const sun = new THREE.DirectionalLight(0xFFFFFF0, 0.5);
 sun.position.set(20, 1000, 10);
 sun.target.position.set(0, 0, 0);
@@ -230,17 +168,6 @@ sun.shadow.camera.bottom = -100;
 game.threeManager.addToScene(sun);
 
 // Cannon-es physics
-// const world = new CANNON.World({
-// 	gravity: new CANNON.Vec3(0, -10, 0), // m/s²
-// });
-
-// // Program contact between floor material and itself
-// const planeMaterial = new CANNON.Material({ friction: 0, });
-// const contactMaterial = new CANNON.ContactMaterial(planeMaterial, planeMaterial,
-// 	{ friction: 0, restitution: 0.1, });
-// world.addContactMaterial(contactMaterial);
-// console.log(world);
-
 // Physical floor
 const planeShape = new CANNON.Plane();
 const planeBody = new CANNON.Body({
@@ -252,20 +179,9 @@ planeBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // make it face up
 planeBody.position.set(0, 0, 0);
 game.cannonManager.world.addBody(planeBody);
 
-// Physical player placeholder
-// const size = 1
-// const halfExtents = new CANNON.Vec3(size, size, size);
-// const boxShape = new CANNON.Box(halfExtents);
-// const boxBody = new CANNON.Body({ mass: 1, shape: boxShape, material: planeMaterial });
-// boxBody.position.set(0, 100, 0);
-// world.addBody(boxBody);
-// player.body = boxBody;
-
+// Creating the player
 game.player.create();
 
-// console.log(game.threeManager);
-const timeStep = 1 / 60; // seconds
-let lastCallTime;
 
 // Rendering loop
 function animate() {
@@ -279,77 +195,16 @@ function animate() {
 	// 	boxBody.velocity.y = 0;
 	// }
 	// done by the game 
-
-	// box.position.copy(boxBody.position);
 	game.player.mesh.position.copy(game.player.body.position);
+
 	// done by cannonManager
 	game.cannonManager.update();
-	// const time = performance.now() / 1000; // seconds
-	// if (!lastCallTime) {
-	// 	world.step(timeStep);
-	// } else {
-	// 	const dt = time - lastCallTime;
-	// 	world.step(timeStep, dt);
-	// }
-	// lastCallTime = time;
 
 	// Player update
-	// done by the player in game
-	// updatePlayer();
 	game.player.update();
 
 	// Finally, render
-	// Done by the threeManager
-	// renderer.render(scene, camera);
 	game.threeManager.render();
 }
-
-// function updatePlayer() {
-// 	if (keyboardController.pressed["w"]) {
-// 		if (player.xAcceleration > player.maxSpeed) {
-// 			player.xAcceleration = player.maxSpeed;
-// 		} else {
-// 			player.xAcceleration += player.acceleration;
-// 		}
-// 	}
-// 	if (keyboardController.pressed["s"]) {
-// 		if (player.xAcceleration < -player.maxSpeed) {
-// 			player.xAcceleration = -player.maxSpeed;
-// 		} else {
-// 			player.xAcceleration -= player.acceleration;
-// 		}
-// 	}
-// 	if (keyboardController.pressed["a"]) {
-// 		if (player.zAcceleration < -player.maxSpeed) {
-// 			player.zAcceleration = -player.maxSpeed;
-// 		} else {
-// 			player.zAcceleration -= player.acceleration;
-// 		}
-// 	}
-// 	if (keyboardController.pressed["d"]) {
-// 		if (player.zAcceleration > player.maxSpeed) {
-// 			player.zAcceleration = player.maxSpeed;
-// 		} else {
-// 			player.zAcceleration += player.acceleration;
-// 		}
-// 	}
-// 	// if (keyboardController.pressed["space"]) {
-// 	// 	console.log(boxBody.position);
-// 	// 	console.log(boxBody.velocity);
-// 	// }
-// 	player.body.velocity.set(player.xAcceleration, player.body.velocity.y, player.zAcceleration);
-// 	dampenAcceleration();
-// 	player.camera.update();
-// }
-
-// function dampenAcceleration() {
-// 	if (keyboardController.hasNoKeysDown()) {
-// 		if (almostZero(player.zAcceleration) && almostZero(player.xAcceleration)) {
-// 			return;
-// 		}
-// 		player.zAcceleration *= player.damping;
-// 		player.xAcceleration *= player.damping;
-// 	}
-// }
 
 animate();
