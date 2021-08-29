9f856488c5f7cb301668a0e1779e22570e1a5cf2
Mitchell Sibal <mitchell.sibal@gmail.com>
2021-08-29 16:41:09 -0700

    added first drafts for needle and enemy classes

---
+++ js/Enemy.js
@@ -0,0 +1,53 @@
+import * as CANNON from './cannon-es.js';
+import { Medy } from './Medy.js';
+import { Needle } from './Needle.js';
+
+export class Enemy extends Medy {
+	// Enemy tracks targetMesh in visual world
+	constructor(targetMesh) {
+		const enemyMesh = new THREE.Mesh(
+			new THREE.BoxGeometry(2, 2, 2),
+			new THREE.MeshLambertMaterial({
+				color: 0xFFFFFF,
+			}));
+		enemyMesh.castShadow = true;
+		enemyMesh.receiveShadow = true;
+		// // TODO raycasting for shooting palms
+		enemyMesh.raycast = function (raycaster, intersects) {
+		}
+		// // Physical player placeholder
+		const size = 1;
+		const halfExtents = new CANNON.Vec3(size, size, size);
+		const boxShape = new CANNON.Box(halfExtents);
+		const enemyBody = new CANNON.Body({
+			mass: 1,
+			shape: boxShape,
+			material: window.game._cannonManager.planeMaterial
+		});
+		enemyBody.addEventListener('collide', function (e) {
+			console.log(e);
+		});
+		super(enemyMesh, enemyBody);
+		this._target = targetMesh;
+		// visual THREE mesh
+		// physics CANNON body
+		// Wrapped in this._mesh and this._body
+	}
+
+	shootPalm() {
+		const targetPoint = this._target.position;
+		const targetVector = new THREE.Vector3();
+		targetVector.subVectors(targetPoint, this._mesh.position);
+		targetVector.normalize();
+
+		const needleShot = new Needle();
+		window.game.addMedy(palmShot);
+		palmShot.setFiringLocation(
+			this._mesh.position.x,
+			this._mesh.position.y - 10,
+			this._mesh.position.z
+		);
+		palmShot.setDirection(targetVector);
+		palmShot.setSpeed(150);
+	}
+}
---
+++ js/Needle.js
@@ -0,0 +1,87 @@
+import * as CANNON from './cannon-es.js';
+import { Medy } from './Medy.js';
+
+export class Needle extends Medy {
+	constructor() {
+		const needleDimensions = new THREE.Vector3(3, 6, 0.5);
+		const needleMesh = new THREE.Mesh(
+			new THREE.BoxGeometry(
+				needleDimensions.x,
+				needleDimensions.y,
+				needleDimensions.z
+			),
+			new THREE.MeshLambertMaterial({
+				color: 0xFFadfb,
+			}));
+		needleMesh.castShadow = true;
+		needleMesh.receiveShadow = true;
+		needleMesh.name = "needle";
+		const halfExtents = new CANNON.Vec3(
+			needleDimensions.x / 2,
+			needleDimensions.y / 2,
+			needleDimensions.z / 2
+		);
+		const needleShape = new CANNON.Box(halfExtents);
+		const needleBody = new CANNON.Body({
+			mass: 1,
+			shape: needleShape,
+			material: window.game._cannonManager.palmMaterial,
+		});
+		super(needleMesh, needleBody);
+
+		this.collisionHandler = this.collide.bind(this);
+		this._body.addEventListener('collide', this.collisionHandler, false);
+		this.updatedWithMass = false;
+	}
+
+	collide(event) {
+		window.game.removeMedy(this);
+	}
+
+	setFiringLocation(location, y, z) {
+		if (location.x === undefined) {
+			this._body.position.set(location, y, z);
+		} else {
+			this._body.position.set(location.x, location.y, location.z);
+		}
+	}
+
+	// accepts either a vector3 or three floats
+	setDirection(direction, y, z) {
+		const testPoint = new THREE.Vector3();
+		if (direction.x === undefined) {
+			this._direction = new THREE.Vector3(direction, y, z);
+			this._body.velocity.set(direction, y, z);
+			testPoint.addVectors(this._mesh.position, new THREE.Vector3(direction, y, z));
+		} else {
+			this._direction = direction.clone();
+			this._body.velocity.set(direction.x, direction.y, direction.z);
+			testPoint.addVectors(this._mesh.position, direction);
+		}
+		this._mesh.lookAt(testPoint);
+		this._body.quaternion.copy(this._mesh.quaternion);
+	}
+
+	setSpeed(speed) {
+		this._speed = speed;
+		this._body.velocity.scale(speed, this._body.velocity);
+	}
+
+	maintainTrajectory() {
+		this._body.velocity.set(
+			this._direction.x,
+			this._direction.y,
+			this._direction.z
+		);
+		this._body.velocity.scale(this._speed, this._body.velocity);
+	}
+
+	update() {
+		super.update();
+		this.maintainTrajectory();
+		if (!this.updatedWithMass) {
+			this.updatedWithMass = true;
+			this._body.mass = 0;
+		}
+	}
+}
